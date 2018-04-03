c15de7041762deccec146a65169e14570e514c9a
Kartik Chawla <kartik.chawla@kelltontech.com>
2018-04-03 18:48:04 +0530

    sample dropdown

--- src/app/reactiveform/reactiveform.component.ts
+++ src/app/reactiveform/reactiveform.component.ts
@@ -7,11 +7,15 @@ import {  ReactiveFormsModule, FormBuilder, FormControl, FormGroup, Validators }
 })
 export class ReactiveformComponent  {
 reactiveForm : FormGroup;
+default : string = "kartik";
+Names = ["kartik", "chawla"];
   constructor(public fb : FormBuilder) {
   	this.reactiveForm = this.fb.group({
   		username : ['', Validators.required],
-  		password : ['', Validators.required]
-  	})
+      password : ['', Validators.required],
+      testNames : [null]
+    });
+    this.reactiveForm.controls['testNames'].setValue(this.default)
    }
 submitReactiveForm()
 {
