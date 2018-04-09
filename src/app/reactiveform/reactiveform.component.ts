364bd4ca857ab9e43f6803914dc6c6e4b1081b89
Kartik Chawla <kartik.chawla@kelltontech.com>
2018-04-09 10:54:06 +0530

    checkbox

--- src/app/reactiveform/reactiveform.component.ts
+++ src/app/reactiveform/reactiveform.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
-import {  ReactiveFormsModule, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {  ReactiveFormsModule, FormBuilder, FormControl, FormGroup, Validators, FormArray } from '@angular/forms';
 @Component({
   selector: 'app-reactiveform',
   templateUrl: './reactiveform.component.html',
   styleUrls: ['./reactiveform.component.css']
 })
 export class ReactiveformComponent  {
+  listArr = ['one','two'];
+  // listArr = new FormArray([
+  //     new FormControl('one'),
+  //     new FormControl('two')
+      
+  // ])
 reactiveForm : FormGroup;
 default : string = "kartik";
 Names = ["kartik", "chawla"];
@@ -13,12 +19,47 @@ Names = ["kartik", "chawla"];
   	this.reactiveForm = this.fb.group({
   		username : ['', Validators.required],
       password : ['', Validators.required],
-      testNames : [null]
+      testNames : [null],
+      list : this.buildList()
     });
     this.reactiveForm.controls['testNames'].setValue(this.default)
+    // this.reactiveForm.controls['list'].setValue(this.listArr[0])
    }
-submitReactiveForm()
+   buildList(){
+     const arr = this.listArr.map(s=>{
+       return this.fb.control(s);
+     })
+     return this.fb.array(arr)
+   }
+submitReactiveForm(event)
 {
+  const formArray: FormArray = this.reactiveForm.get('list') as FormArray;  
+  let i: number = 0;  
+  Object.values(event.target).forEach(elemt=>{
+    if(elemt.type == 'checkbox'){
+      if(elemt.checked){
+        if(formArray.controls.length>0){
+          formArray.controls.forEach((ctrl: FormControl) => {
+            if(ctrl.value == elemt.value) {
+              formArray.removeAt(i);
+              return;
+            }
+            i++;
+          });
+        }
+        formArray.push(new FormControl(elemt.value));
+      }else{
+        i = 0;
+            formArray.controls.forEach((ctrl: FormControl) => {
+              if(ctrl.value == elemt.value) {
+                formArray.removeAt(i);
+                return;
+              }
+              i++;
+            });
+      }
+    }
+  })
 	console.log(this.reactiveForm.value);
 }
 
